f54a3c817a4d7c0a379bdfc13063cc6280b6a3c5
babicoolhp <huynguyenminh.1106@gmail.com>
2023-01-12 21:24:34 +0700

    Sprint 5: Add disk controller

--- server/src/controllers/disk.controller.js
+++ server/src/controllers/disk.controller.js
@@ -4,10 +4,8 @@ const CustomErrorHandler = require("../middlewares/CustomErrorHandler");
 
 exports.createDisk = async (req, res, next) => {
     try {
-        const { type, message, statusCode } = await diskService.createDisk(
-            req.body,
-            req.file
-        );
+        const { type, message, statusCode, disk } =
+            await diskService.createDisk(req.body, req.file);
 
         if (type === statusType.error)
             return next(new CustomErrorHandler(statusCode, message));
@@ -15,6 +13,7 @@ exports.createDisk = async (req, res, next) => {
         res.status(statusCode).json({
             type,
             message,
+            disk,
         });
     } catch (err) {
         next(err);
@@ -33,6 +32,60 @@ exports.getListOfDisk = async (req, res, next) => {
             type,
             message,
             disks,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.searchDisk = async (req, res, next) => {
+    try {
+        const { type, message, statusCode, disks } =
+            await diskService.searchDisk(req.query.name);
+
+        if (type === statusType.error)
+            return next(new CustomErrorHandler(statusCode, message));
+
+        res.status(statusCode).json({
+            type,
+            message,
+            disks,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.deleteDisk = async (req, res, next) => {
+    try {
+        const { type, statusCode, message } = await diskService.deleteDisk(
+            req.params.id
+        );
+
+        if (type === statusType.error)
+            return next(new CustomErrorHandler(statusCode, message));
+
+        res.status(statusCode).json({
+            type,
+            message,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.updateDisk = async (req, res, next) => {
+    try {
+        const { type, statusCode, message, disk } =
+            await diskService.updateDisk(req.params.id, req.body, req.file);
+
+        if (type === statusType.error)
+            return next(new CustomErrorHandler(statusCode, message));
+
+        res.status(statusCode).json({
+            type,
+            message,
+            disk,
         });
     } catch (err) {
         next(err);
