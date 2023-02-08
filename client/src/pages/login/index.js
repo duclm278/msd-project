f02ae3742bca57a61361b0c6dbda4ce008aaaa84
duclm278 <palered548@gmail.com>
2023-02-08 23:53:45 +0700

    Sprint 8: Fix table CSS

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -16,121 +16,111 @@ import { UserContext } from "../../contexts/UserProvider";
 import Loading from "../../components/Loading";
 
 export default function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const { setUser } = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const login = async () => {
-        if (!email || !password) {
-            setError("Email or password is empty!");
-            return;
-        }
+  const login = async () => {
+    if (!email || !password) {
+      setError("Email or password is empty!");
+      return;
+    }
 
-        setLoading(true);
-        try {
-            const response = await employeeApi.login({ email, password });
-            if (response?.data?.type === status.success) {
-                authentication.login(response?.data?.employee.employee_id);
-                setUser(response?.data?.employee);
-                setLoading(false);
-                navigate("/home");
-            }
-        } catch (err) {
-            if (err.response?.data?.type === status.error) {
-                setError(err.response?.data?.message);
-            }
-            setLoading(false);
-        }
-    };
+    setLoading(true);
+    try {
+      const response = await employeeApi.login({ email, password });
+      if (response?.data?.type === status.success) {
+        authentication.login(response?.data?.employee.employee_id);
+        setUser(response?.data?.employee);
+        setLoading(false);
+        navigate("/home");
+      }
+    } catch (err) {
+      if (err.response?.data?.type === status.error) {
+        setError(err.response?.data?.message);
+      }
+      setLoading(false);
+    }
+  };
 
-    const handleLoginEnter = (e) => {
-        if (e.key === "Enter") login();
-    };
+  const handleLoginEnter = (e) => {
+    if (e.key === "Enter") login();
+  };
 
-    const handleLogin = () => {
-        login();
-    };
+  const handleLogin = () => {
+    login();
+  };
 
-    return (
-        <>
-            <Box
-                sx={{
-                    height: "100vh",
-                    overflow: "hidden",
-                }}
+  return (
+    <>
+      <Box
+        sx={{
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        {loading && <Loading />}
+        {!loading && (
+          <div onKeyDown={handleLoginEnter}>
+            <Sheet
+              variant="outlined"
+              sx={{
+                width: 450,
+                mx: "auto",
+                my: 4,
+                py: 3,
+                px: 2,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                borderRadius: "sm",
+                boxShadow: "md",
+              }}
             >
-                {loading && <Loading />}
-                {!loading && (
-                    <div onKeyDown={handleLoginEnter}>
-                        <Sheet
-                            variant="outlined"
-                            sx={{
-                                width: 450,
-                                mx: "auto",
-                                my: 4,
-                                py: 3,
-                                px: 2,
-                                display: "flex",
-                                flexDirection: "column",
-                                gap: 2,
-                                borderRadius: "sm",
-                                boxShadow: "md",
-                            }}
-                        >
-                            <div>
-                                <Typography level="h4" component="h1">
-                                    Welcome!
-                                </Typography>
-                                <Typography level="body2">
-                                    Sign in to continue.
-                                </Typography>
-                            </div>
-                            <FormControl>
-                                <FormLabel>Username</FormLabel>
-                                <Input
-                                    value={email}
-                                    name="email"
-                                    type="email"
-                                    placeholder="email"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Password</FormLabel>
-                                <Input
-                                    value={password}
-                                    name="password"
-                                    type="password"
-                                    placeholder="password"
-                                    onChange={(e) =>
-                                        setPassword(e.target.value)
-                                    }
-                                />
-                            </FormControl>
-                            {error && (
-                                <span className="text-red-600">{error}</span>
-                            )}
-                            <Button onClick={() => handleLogin()}>
-                                Log in
-                            </Button>
-                            <Typography
-                                endDecorator={
-                                    <Link href="/signup">Sign up</Link>
-                                }
-                                fontSize="sm"
-                                sx={{ alignSelf: "center" }}
-                            >
-                                Don't have an account?
-                            </Typography>
-                        </Sheet>
-                    </div>
-                )}
-            </Box>
-        </>
-    );
+              <div>
+                <Typography level="h4" component="h1">
+                  Welcome!
+                </Typography>
+                <Typography level="body2">Sign in to continue.</Typography>
+              </div>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  value={email}
+                  name="email"
+                  type="email"
+                  placeholder="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  value={password}
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+              {error && <span className="text-red-600">{error}</span>}
+              <Button onClick={() => handleLogin()}>Log in</Button>
+              <Typography
+                endDecorator={<Link href="/signup">Sign up</Link>}
+                fontSize="sm"
+                sx={{ alignSelf: "center" }}
+              >
+                Don't have an account?
+              </Typography>
+            </Sheet>
+          </div>
+        )}
+      </Box>
+    </>
+  );
 }
--- client/src/pages/tables/TableDialogAdd.js
+++ client/src/pages/tables/TableDialogAdd.js
@@ -19,129 +19,118 @@ import tableApi from "../../api/tableApi";
 import status from "../../constants/status";
 
 export default function TableDialogAdd({
-    open,
-    setOpen,
-    setLoading,
-    fetchData,
+  open,
+  setOpen,
+  setLoading,
+  fetchData,
 }) {
-    const [id, setId] = useState("");
-    const [numberOfSeats, setNumberOfSeats] = useState("");
-    const [tableStatus, setTableStatus] = useState(filterOpts[0].status);
-    const { enqueueSnackbar } = useSnackbar();
+  const [id, setId] = useState("");
+  const [numberOfSeats, setNumberOfSeats] = useState("");
+  const [tableStatus, setTableStatus] = useState(filterOpts[0].status);
+  const { enqueueSnackbar } = useSnackbar();
 
-    const handleSubmit = () => {
-        const submit = async () => {
-            setLoading(true);
-            try {
-                const response = await tableApi.createTable({
-                    id,
-                    numberOfSeats,
-                    tableStatus,
-                });
-                if (response?.data?.type === status.success) {
-                    fetchData();
-                    setLoading(false);
-                    enqueueSnackbar(response?.data?.message, {
-                        variant: "success",
-                    });
-                }
-            } catch (err) {
-                setLoading(false);
-                enqueueSnackbar(err.response?.data?.message, {
-                    variant: "error",
-                });
-            }
-            setId("");
-            setNumberOfSeats("");
-            setTableStatus(filterOpts[0].status);
-        };
-
-        submit();
+  const handleSubmit = () => {
+    const submit = async () => {
+      setLoading(true);
+      try {
+        const response = await tableApi.createTable({
+          id,
+          numberOfSeats,
+          tableStatus,
+        });
+        if (response?.data?.type === status.success) {
+          fetchData();
+          setLoading(false);
+          enqueueSnackbar(response?.data?.message, {
+            variant: "success",
+          });
+        }
+      } catch (err) {
+        setLoading(false);
+        enqueueSnackbar(err.response?.data?.message, {
+          variant: "error",
+        });
+      }
+      setId("");
+      setNumberOfSeats("");
+      setTableStatus(filterOpts[0].status);
     };
 
-    return (
-        <>
-            <Modal open={open} onClose={() => setOpen(false)}>
-                <ModalDialog
-                    sx={{
-                        maxWidth: 500,
-                        borderRadius: "md",
-                        p: 3,
-                        boxShadow: "lg",
-                    }}
+    submit();
+  };
+
+  return (
+    <>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalDialog
+          sx={{
+            maxWidth: 500,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+          }}
+        >
+          <ModalClose />
+          <Typography
+            component="h2"
+            level="inherit"
+            fontSize="1.25em"
+            mb="0.25em"
+          >
+            Add new table
+          </Typography>
+          <Stack
+            component="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+              setOpen(false);
+            }}
+          >
+            <Stack spacing={2}>
+              <FormControl required>
+                <FormLabel>ID</FormLabel>
+                <Input
+                  autoFocus
+                  name="id"
+                  placeholder="ID"
+                  value={id}
+                  onChange={(e) => setId(e.target.value)}
+                />
+              </FormControl>
+              <FormControl required>
+                <FormLabel>Capacity</FormLabel>
+                <Input
+                  name="capacity"
+                  placeholder="Number of seats"
+                  value={numberOfSeats}
+                  onChange={(e) => setNumberOfSeats(e.target.value)}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  value={tableStatus}
+                  onChange={(e, newTableStatus) => {
+                    setTableStatus(newTableStatus);
+                  }}
                 >
-                    <ModalClose />
-                    <Typography
-                        component="h2"
-                        level="inherit"
-                        fontSize="1.25em"
-                        mb="0.25em"
-                    >
-                        Add new table
-                    </Typography>
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            handleSubmit();
-                            setOpen(false);
-                        }}
-                    >
-                        <Stack>
-                            <Stack spacing={2}>
-                                <FormControl required>
-                                    <FormLabel>ID</FormLabel>
-                                    <Input
-                                        name="id"
-                                        placeholder="ID"
-                                        autoFocus
-                                        value={id}
-                                        onChange={(e) => setId(e.target.value)}
-                                    />
-                                </FormControl>
-                                <FormControl required>
-                                    <FormLabel>Capacity</FormLabel>
-                                    <Input
-                                        name="capacity"
-                                        placeholder="Number of seats"
-                                        value={numberOfSeats}
-                                        onChange={(e) =>
-                                            setNumberOfSeats(e.target.value)
-                                        }
-                                    />
-                                </FormControl>
-                                <FormControl>
-                                    <FormLabel>Status</FormLabel>
-                                    <Select
-                                        value={tableStatus}
-                                        onChange={(e, newTableStatus) => {
-                                            setTableStatus(newTableStatus);
-                                        }}
-                                    >
-                                        {filterOpts.map((filterOpt) => (
-                                            <Option
-                                                key={filterOpt}
-                                                value={filterOpt.status}
-                                            >
-                                                {filterOpt.status}
-                                            </Option>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            </Stack>
-                            <Box
-                                mt={3}
-                                display="flex"
-                                gap={2}
-                                sx={{ width: "100%" }}
-                            >
-                                <Button type="submit" sx={{ flex: 1 }}>
-                                    Save
-                                </Button>
-                            </Box>
-                        </Stack>
-                    </form>
-                </ModalDialog>
-            </Modal>
-        </>
-    );
+                  {filterOpts.map((filterOpt) => (
+                    <Option key={filterOpt} value={filterOpt.status}>
+                      {filterOpt.status}
+                    </Option>
+                  ))}
+                </Select>
+              </FormControl>
+            </Stack>
+            <Box mt={3} display="flex" gap={2} sx={{ width: "100%" }}>
+              <Button type="submit" sx={{ flex: 1 }}>
+                Save
+              </Button>
+            </Box>
+          </Stack>
+        </ModalDialog>
+      </Modal>
+    </>
+  );
 }
