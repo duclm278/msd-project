2e78fe69a5b63801e6bdeb2e8c580dee97a36aa0
Quang <quang29112002@gmail.com>
2023-02-17 00:43:09 +0700

    file sql to create db

--- restaurant.sql
+++ restaurant.sql
@@ -25,36 +25,6 @@ SET row_security = off;
 
 ALTER SCHEMA public OWNER TO postgres;
 
---
--- Name: calculate_total_order_price(); Type: FUNCTION; Schema: public; Owner: postgres
---
-
-CREATE FUNCTION public.calculate_total_order_price() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
-    UPDATE "Order" 
-        SET total_cost = 
-			(SELECT SUM(price * DIO.quantity)
-             FROM Disk D
-			 inner join DiskInOrder DIO
-				 on D.disk_id = DIO.disk_id
-			 WHERE DIO.order_id = NEW.order_id)
-			 +
-			(SELECT SUM(combo_price * CIO.quantity)
-			 FROM Combo C
-			 inner join ComboInOrder CIO
-				 on C.combo_id = CIO.combo_id
-			 WHERE CIO.order_id = NEW.order_id)
-         WHERE order_id = NEW.order_id;
-
-    RETURN NEW;
-END;
-$$;
-
-
-ALTER FUNCTION public.calculate_total_order_price() OWNER TO postgres;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -68,9 +38,12 @@ CREATE TABLE public."Order" (
     customer_id integer,
     table_id integer,
     reserved_time timestamp with time zone,
-    total_cost double precision,
+    total_cost_before_discount double precision,
     phone character varying(11) DEFAULT NULL::character varying,
-    customer_name character varying(50)
+    customer_name character varying(50),
+    status character varying(20),
+    event_id integer,
+    total_cost_after_discount double precision
 );
 
 
@@ -228,8 +201,7 @@ CREATE TABLE public.customer (
     email character varying(60),
     phone character varying(11),
     point integer DEFAULT 1,
-    rank_id integer DEFAULT 1,
-    CONSTRAINT customer_email_check CHECK (((email)::text ~~ '%_@__%.__%'::text))
+    rank_id integer DEFAULT 1
 );
 
 
@@ -499,7 +471,19 @@ ALTER TABLE ONLY public.rank ALTER COLUMN rank_id SET DEFAULT nextval('public.ra
 -- Data for Name: Order; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Order" (order_id, customer_id, table_id, reserved_time, total_cost, phone, customer_name) FROM stdin;
+COPY public."Order" (order_id, customer_id, table_id, reserved_time, total_cost_before_discount, phone, customer_name, status, event_id, total_cost_after_discount) FROM stdin;
+25	\N	7	2023-02-11 16:05:00+00	170000	0985832741	Phạm Vương Tú	Paid	24	155000
+19	\N	11	2023-02-14 00:00:00+00	255000	0905673854	Nguyễn Phương Quang	Paid	22	180000
+22	\N	5	2023-02-16 00:00:00+00	65000	0981212843	Phạm Thảo Nhi	Paid	\N	65000
+28	19	12	2023-02-16 00:00:00+00	450000	0989893472	Nguyễn Minh Huyền	Unpaid	25	420000
+21	\N	2	2023-02-16 00:00:00+00	38000	0937284723	Nguyễn Tuấn Hiệp	Paid	\N	38000
+17	\N	3	2023-02-09 00:00:00+00	220000	0867658342	Lê Minh Đức	Paid	25	190000
+18	\N	1	2023-02-17 00:00:00+00	300000	0937284723	Nguyễn Tuấn Hiệp	Paid	21	250000
+27	\N	10	2023-02-06 17:03:00+00	470000	0909422556	Phạm Khánh Linh	Paid	24	455000
+20	\N	3	2023-02-10 00:00:00+00	235000	0128473628	Nguyễn Hoàng Hải	Paid	\N	235000
+26	16	8	2023-02-20 00:00:00+00	275000	0985832741	Phạm Vương Tú	Unpaid	21	225000
+23	\N	10	2023-02-08 00:00:00+00	70000	0987654321	Đỗ Tuấn Minh	Paid	\N	70000
+24	\N	12	2023-01-20 16:00:00+00	850000	0989878767	Nguyễn Trần Minh Ngọc	Paid	25	820000
 \.
 
 
@@ -508,13 +492,18 @@ COPY public."Order" (order_id, customer_id, table_id, reserved_time, total_cost,
 --
 
 COPY public."Table" (table_id, number_of_seats, table_status) FROM stdin;
-3	2	Reserved
-5	5	Occupied
-1	4	Available
-7	4	Available
-2	4	Available
-8	3	Available
-6	2	Available
+1	5	Available
+2	8	Reserved
+3	6	Occupied
+4	2	Out of Order
+5	2	Occupied
+6	8	Out of Order
+7	8	Reserved
+8	10	Available
+10	8	Available
+9	2	Out of Order
+11	6	Reserved
+12	4	Available
 \.
 
 
@@ -536,10 +525,11 @@ COPY public.category (category_id, category_name) FROM stdin;
 --
 
 COPY public.combo (combo_id, combo_name, combo_price, description, image, image_id) FROM stdin;
-12	Test combo	13	nooo	https://res.cloudinary.com/dejdoneih/image/upload/v1675922604/restaurant/Combos/Test-combo/n7k0iwnvcb2jujl2z0yv.png	restaurant/Combos/Test-combo/n7k0iwnvcb2jujl2z0yv
-13	Opening ceremony	500000	Hello	https://res.cloudinary.com/dejdoneih/image/upload/v1675952675/restaurant/Combos/Opening-ceremony/eiigctjclnphvuxglaal.png	restaurant/Combos/Opening-ceremony/eiigctjclnphvuxglaal
-10	Summer Combo	520030	Chào đón mùa hè bất tận	https://res.cloudinary.com/dejdoneih/image/upload/v1675870825/restaurant/Combos/Summer-Combo/anep3bg6dst3bpaguvxx.png	restaurant/Combos/Summer-Combo/anep3bg6dst3bpaguvxx
-9	Winter combo	130000	Combo for a winter with love	https://res.cloudinary.com/dejdoneih/image/upload/v1675877083/restaurant/Combos/Winter-combo/wj7dcypzriu3tedk7s5g.png	restaurant/Combos/Winter-combo/wj7dcypzriu3tedk7s5g
+33	Lễ tạ ơn	270000		https://res.cloudinary.com/dejdoneih/image/upload/v1676564268/restaurant/Combos/L%E1%BB%85-t%E1%BA%A1-%C6%A1n/ox8h0d7m77cagciaqxjm.png	restaurant/Combos/Lễ-tạ-ơn/ox8h0d7m77cagciaqxjm
+35	Giáng sinh	180000		https://res.cloudinary.com/dejdoneih/image/upload/v1676564482/restaurant/Combos/Gi%C3%A1ng-sinh/sbrijog237fajaoxni4e.png	restaurant/Combos/Giáng-sinh/sbrijog237fajaoxni4e
+34	Năm mới	180000		https://res.cloudinary.com/dejdoneih/image/upload/v1676567735/restaurant/Combos/N%C4%83m-m%E1%BB%9Bi/duatekt3cw4gcyxijwbq.png	restaurant/Combos/Năm-mới/duatekt3cw4gcyxijwbq
+32	Halloween	100000		https://res.cloudinary.com/dejdoneih/image/upload/v1676567861/restaurant/Combos/Halloween/qx5qr7s1xjmwpyompoog.png	restaurant/Combos/Halloween/qx5qr7s1xjmwpyompoog
+31	Lễ tình nhân	150000		https://res.cloudinary.com/dejdoneih/image/upload/v1676561496/restaurant/Combos/Valentine-Combo/iufltpf8kuuvctmiiksf.png	restaurant/Combos/Valentine-Combo/iufltpf8kuuvctmiiksf
 \.
 
 
@@ -548,6 +538,15 @@ COPY public.combo (combo_id, combo_name, combo_price, description, image, image_
 --
 
 COPY public.comboinorder (order_id, combo_id, quantity) FROM stdin;
+20	35	1
+26	35	1
+24	31	5
+19	34	1
+28	31	3
+17	31	1
+18	33	1
+27	33	1
+27	32	2
 \.
 
 
@@ -556,9 +555,19 @@ COPY public.comboinorder (order_id, combo_id, quantity) FROM stdin;
 --
 
 COPY public.customer (customer_id, name, email, phone, point, rank_id) FROM stdin;
-1	Nguyen Phuong Quang	quang29112002@gmail.com	0329715851	1000	1
-5	Tạ Hương Giang	\N	0982255660	100	4
-6	Duc Le	duc.lm200164@sis.hust.edu.vn	9999999999	124	1
+11	Lê Minh Đức	\N	0867658342	120	4
+12	Nguyễn Tuấn Hiệp	\N	0937284723	180	3
+10	Nguyễn Phương Quang	null	0905673854	100	5
+13	Nguyễn Hoàng Hải	\N	0128473628	200	2
+14	Nguyễn Minh Huy	\N	0847395677	150	3
+15	Đỗ Tuấn Minh	\N	0987654321	100	3
+16	Phạm Vương Tú	\N	0985832741	10	1
+17	Lương Nam Khánh	\N	0348127342	500	4
+18	Phạm Thảo Nhi	\N	0981212843	300	2
+19	Nguyễn Minh Huyền	\N	0989893472	250	4
+20	Phạm Khánh Linh	\N	0909422556	140	1
+21	Nguyễn Trần Minh Ngọc	\N	0989878767	220	1
+22	Nguyễn Thị Phương Thảo	\N	0901234141	120	2
 \.
 
 
@@ -567,13 +576,34 @@ COPY public.customer (customer_id, name, email, phone, point, rank_id) FROM stdi
 --
 
 COPY public.disk (disk_id, disk_name, description, price, image, category_id, image_id) FROM stdin;
-8	Coca Cola	Gas drink	15	https://res.cloudinary.com/dejdoneih/image/upload/v1673448694/restaurant/Disks/Coca-Cola/ivtzonhtxn0i5augilkh.png	5	restaurant/Disks/Coca-Cola/ivtzonhtxn0i5augilkh
-9	Pepsi	Gas drink	15	https://res.cloudinary.com/dejdoneih/image/upload/v1673449735/restaurant/Disks/Pepsi/jrcoznwqjiykwedbpeoe.png	5	restaurant/Disks/Pepsi/jrcoznwqjiykwedbpeoe
-6	Sashimi	raw salmon	12	https://res.cloudinary.com/dejdoneih/image/upload/v1673453604/restaurant/Disks/Sashimi/lcysgnylebeycitfywyv.png	3	restaurant/Disks/Sashimi/lcysgnylebeycitfywyv
-11	Tiramisu cake	Dessert cake	450000	https://res.cloudinary.com/dejdoneih/image/upload/v1675785133/restaurant/Disks/Tiramisu-cake/scmdsr3urce3wj9heok2.png	4	restaurant/Disks/Tiramisu-cake/scmdsr3urce3wj9heok2
-7	Sushi	raw salmon	150000	https://res.cloudinary.com/dejdoneih/image/upload/v1673448568/restaurant/Disks/Sushi/lj0rx6kabff8p7xe9adt.png	3	restaurant/Disks/Sushi/lj0rx6kabff8p7xe9adt
-12	Pho Bo	National food	36000	https://res.cloudinary.com/dejdoneih/image/upload/v1675785200/restaurant/Disks/Pho-Bo/uh9osqj5tbynw3fmkga4.png	2	restaurant/Disks/Pho-Bo/uh9osqj5tbynw3fmkga4
-13	final version	dasd	120000	https://res.cloudinary.com/dejdoneih/image/upload/v1675925714/restaurant/Disks/final-version/wevozefr46dddjihxpao.png	1	restaurant/Disks/final-version/wevozefr46dddjihxpao
+25	Salad		35000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562011/restaurant/Disks/Salad/gg6f1sck2twnclsxn9sc.png	1	restaurant/Disks/Salad/gg6f1sck2twnclsxn9sc
+26	Súp bí ngô		25000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562078/restaurant/Disks/S%C3%BAp-b%C3%AD-ng%C3%B4/p2y6mpkmvkxqmg8m99mb.png	1	restaurant/Disks/Súp-bí-ngô/p2y6mpkmvkxqmg8m99mb
+27	Súp ngũ cốc		20000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562110/restaurant/Disks/S%C3%BAp-ng%C5%A9-c%E1%BB%91c/pxvfl2joogzkvqpwtdmd.png	1	restaurant/Disks/Súp-ngũ-cốc/pxvfl2joogzkvqpwtdmd
+28	Coca		10000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562135/restaurant/Disks/Coca/sgfkpzzqw56xjd5ktrtg.png	5	restaurant/Disks/Coca/sgfkpzzqw56xjd5ktrtg
+18	Phở bò		60000	https://res.cloudinary.com/dejdoneih/image/upload/v1676560212/restaurant/Disks/Ph%E1%BB%9F-b%C3%B2/mkym0abt9h3lobcelqgb.png	3	restaurant/Disks/Phở-bò/mkym0abt9h3lobcelqgb
+29	Bánh macaroon		25000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562204/restaurant/Disks/B%C3%A1nh-macaroon/wcjavkpjcslmhfujaxfy.png	4	restaurant/Disks/Bánh-macaroon/wcjavkpjcslmhfujaxfy
+30	Bún chả		35000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562237/restaurant/Disks/B%C3%BAn-ch%E1%BA%A3/zerdlrvlygq6tu9ngbe7.png	2	restaurant/Disks/Bún-chả/zerdlrvlygq6tu9ngbe7
+19	Bún riêu		40000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562282/restaurant/Disks/B%C3%BAn-ri%C3%AAu/l9tp2wbaozckdzbmoyvi.png	3	restaurant/Disks/Bún-riêu/l9tp2wbaozckdzbmoyvi
+31	Nước suối		8000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562320/restaurant/Disks/N%C6%B0%E1%BB%9Bc-su%E1%BB%91i/cflvtkr5ybd2fzesdxl7.png	5	restaurant/Disks/Nước-suối/cflvtkr5ybd2fzesdxl7
+32	Bánh pancake		35000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562359/restaurant/Disks/B%C3%A1nh-pancake/pfeoo1fikuh5ipy0pm7h.png	4	restaurant/Disks/Bánh-pancake/pfeoo1fikuh5ipy0pm7h
+20	Xôi thịt	Xôi thịt lợn	25000	https://res.cloudinary.com/dejdoneih/image/upload/v1676560357/restaurant/Disks/X%C3%B4i-th%E1%BB%8Bt/tr3jutu8tcealnnfg6mn.png	2	restaurant/Disks/Xôi-thịt/tr3jutu8tcealnnfg6mn
+33	Nem		25000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562393/restaurant/Disks/Nem/fz3tbsmjkalsvurg7vhg.png	3	restaurant/Disks/Nem/fz3tbsmjkalsvurg7vhg
+21	Bánh mỳ		15000	https://res.cloudinary.com/dejdoneih/image/upload/v1676560414/restaurant/Disks/B%C3%A1nh-m%E1%BB%B3/ixnnjyxqvmczybql7kji.png	2	restaurant/Disks/Bánh-mỳ/ixnnjyxqvmczybql7kji
+34	Soju		18000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562426/restaurant/Disks/Soju/ddhtdeqimni6halegkzs.png	5	restaurant/Disks/Soju/ddhtdeqimni6halegkzs
+35	Gà rán		50000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562471/restaurant/Disks/G%C3%A0-r%C3%A1n/rcvwtrjers3mk6ovbocc.png	3	restaurant/Disks/Gà-rán/rcvwtrjers3mk6ovbocc
+36	Mỳ vằn thắn		40000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562510/restaurant/Disks/M%E1%BB%B3-v%E1%BA%B1n-th%E1%BA%AFn/imyt4plvnyor5gjelpie.png	2	restaurant/Disks/Mỳ-vằn-thắn/imyt4plvnyor5gjelpie
+23	Bia		15000	https://res.cloudinary.com/dejdoneih/image/upload/v1676561205/restaurant/Disks/Bia/acanh8vwhwod7bo0dkx3.png	5	restaurant/Disks/Bia/acanh8vwhwod7bo0dkx3
+37	Cơm rang		30000	https://res.cloudinary.com/dejdoneih/image/upload/v1676562577/restaurant/Disks/C%C6%A1m-rang/sbdrl9zdo72voqtcwiu2.png	3	restaurant/Disks/Cơm-rang/sbdrl9zdo72voqtcwiu2
+38	Bánh cuốn		30000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563106/restaurant/Disks/B%C3%A1nh-cu%E1%BB%91n/uz22alor2cwfcwnbmcuc.png	2	restaurant/Disks/Bánh-cuốn/uz22alor2cwfcwnbmcuc
+40	Bánh truffle		20000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563165/restaurant/Disks/B%C3%A1nh-truffle/tr1ofuvhuiuvlgshrzho.png	4	restaurant/Disks/Bánh-truffle/tr1ofuvhuiuvlgshrzho
+41	Bánh pudding		25000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563195/restaurant/Disks/B%C3%A1nh-pudding/bvp7tehtwde1bekbfr2t.png	4	restaurant/Disks/Bánh-pudding/bvp7tehtwde1bekbfr2t
+42	Kem ốc quế		15000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563220/restaurant/Disks/Kem-%E1%BB%91c-qu%E1%BA%BF/mfpxsavipmxe4f0jmypf.png	4	restaurant/Disks/Kem-ốc-quế/mfpxsavipmxe4f0jmypf
+43	Trà sữa		35000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563250/restaurant/Disks/Tr%C3%A0-s%E1%BB%AFa/vfgmrsvlm4no5udplax1.png	5	restaurant/Disks/Trà-sữa/vfgmrsvlm4no5udplax1
+44	Hoa quả tươi		30000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563288/restaurant/Disks/Hoa-qu%E1%BA%A3-t%C6%B0%C6%A1i/whirudhsijeic1gtr1a6.png	4	restaurant/Disks/Hoa-quả-tươi/whirudhsijeic1gtr1a6
+45	Sashimi		30000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563995/restaurant/Disks/Sashimi/zmbcaogfmaxlwgvonxre.png	1	restaurant/Disks/Sashimi/zmbcaogfmaxlwgvonxre
+46	Salad hoa quả		25000	https://res.cloudinary.com/dejdoneih/image/upload/v1676564036/restaurant/Disks/Salad-hoa-qu%E1%BA%A3/dlr6hb5ex0trvpo89j3y.png	1	restaurant/Disks/Salad-hoa-quả/dlr6hb5ex0trvpo89j3y
+39	Caramel		20000	https://res.cloudinary.com/dejdoneih/image/upload/v1676563134/restaurant/Disks/Caremel/x12yh1atxq3iyyf3ron9.png	4	restaurant/Disks/Caremel/x12yh1atxq3iyyf3ron9
+47	Whisky		30000	https://res.cloudinary.com/dejdoneih/image/upload/v1676567793/restaurant/Disks/Whisky/cettcvsyxbigcvgmrm2d.png	5	restaurant/Disks/Whisky/cettcvsyxbigcvgmrm2d
 \.
 
 
@@ -582,19 +612,33 @@ COPY public.disk (disk_id, disk_name, description, price, image, category_id, im
 --
 
 COPY public.diskincombo (combo_id, disk_id, quantity) FROM stdin;
-12	8	1
-12	9	1
-13	8	1
-13	9	1
-13	6	1
-13	11	1
-13	13	1
-10	8	2
-10	12	2
-10	11	1
-9	8	2
-9	6	1
-9	7	1
+32	26	2
+32	33	1
+32	40	1
+32	41	1
+31	29	1
+31	21	1
+31	23	1
+31	41	1
+31	46	1
+31	47	2
+33	35	4
+33	45	1
+33	29	1
+33	34	1
+33	28	2
+35	25	2
+35	42	1
+35	41	2
+35	27	1
+35	45	1
+35	40	1
+34	27	3
+34	29	1
+34	31	1
+34	33	1
+34	23	2
+34	44	2
 \.
 
 
@@ -603,6 +647,30 @@ COPY public.diskincombo (combo_id, disk_id, quantity) FROM stdin;
 --
 
 COPY public.diskinorder (order_id, disk_id, quantity) FROM stdin;
+21	31	1
+21	38	1
+17	40	1
+17	44	1
+17	39	1
+18	38	1
+20	40	1
+20	42	1
+20	39	1
+26	21	1
+26	37	1
+26	35	1
+23	33	1
+23	40	1
+23	41	1
+24	27	5
+25	25	2
+25	26	3
+25	46	1
+19	26	1
+19	25	1
+19	21	1
+22	36	1
+22	41	1
 \.
 
 
@@ -621,8 +689,11 @@ COPY public.employee (employee_id, name, email, phone, address, password) FROM s
 --
 
 COPY public.event (event_id, event_name, description, event_status, poster, begin_time, end_time, poster_id, discount, min_cost) FROM stdin;
-11	Summer Combo	Testttt	Available	https://res.cloudinary.com/dejdoneih/image/upload/v1675917451/restaurant/Events/Summer-Combo/a8se3e0vpnxd4dsdqeux.png	2023-02-09 00:00:00+00	2023-02-24 00:00:00+00	restaurant/Events/Summer-Combo/a8se3e0vpnxd4dsdqeux	20000	50000
-12	Buzz	.	Available	https://res.cloudinary.com/dejdoneih/image/upload/v1675952598/restaurant/Events/Buzz/lhgce86oeojt3veors8r.png	2023-02-09 00:00:00+00	2023-02-09 00:00:00+00	restaurant/Events/Buzz/lhgce86oeojt3veors8r	123	123
+21	Giáng sinh			https://res.cloudinary.com/dejdoneih/image/upload/v1676564604/restaurant/Events/Gi%C3%A1ng-sinh/zwzxa9svwlz86woxiajj.png	2023-12-16 00:00:00+00	2023-12-30 00:00:00+00	restaurant/Events/Giáng-sinh/zwzxa9svwlz86woxiajj	50000	200000
+22	Năm mới			https://res.cloudinary.com/dejdoneih/image/upload/v1676564782/restaurant/Events/N%C4%83m-m%E1%BB%9Bi/m2i2sgimvmkyyxjdnlag.png	2022-12-30 00:00:00+00	2023-01-15 00:00:00+00	restaurant/Events/Năm-mới/m2i2sgimvmkyyxjdnlag	75000	400000
+23	Halloween			https://res.cloudinary.com/dejdoneih/image/upload/v1676564853/restaurant/Events/Halloween/bo6kwprmbvmrr2cmoeoc.png	2023-10-01 00:00:00+00	2023-10-31 00:00:00+00	restaurant/Events/Halloween/bo6kwprmbvmrr2cmoeoc	20000	100000
+25	Lễ tình nhân			https://res.cloudinary.com/dejdoneih/image/upload/v1676564985/restaurant/Events/L%E1%BB%85-t%C3%ACnh-nh%C3%A2n/otxt3pbbus3vvgodozjk.png	2023-02-13 00:00:00+00	2023-02-15 00:00:00+00	restaurant/Events/Lễ-tình-nhân/otxt3pbbus3vvgodozjk	30000	180000
+24	Lễ phục sinh			https://res.cloudinary.com/dejdoneih/image/upload/v1676564933/restaurant/Events/L%E1%BB%85-Ph%E1%BB%A5c-sinh/ey5wtt8roxxtlnyqs2al.png	2023-03-22 00:00:00+00	2023-04-25 00:00:00+00	restaurant/Events/Lễ-Phục-sinh/ey5wtt8roxxtlnyqs2al	15000	80000
 \.
 
 
@@ -643,7 +714,7 @@ COPY public.rank (rank_id, rank, lower_bound, higher_bound) FROM stdin;
 -- Name: Order_order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public."Order_order_id_seq"', 2, true);
+SELECT pg_catalog.setval('public."Order_order_id_seq"', 28, true);
 
 
 --
@@ -664,21 +735,21 @@ SELECT pg_catalog.setval('public.category_category_id_seq', 5, true);
 -- Name: combo_combo_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.combo_combo_id_seq', 13, true);
+SELECT pg_catalog.setval('public.combo_combo_id_seq', 35, true);
 
 
 --
 -- Name: customer_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.customer_customer_id_seq', 6, true);
+SELECT pg_catalog.setval('public.customer_customer_id_seq', 22, true);
 
 
 --
 -- Name: disk_disk_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.disk_disk_id_seq', 13, true);
+SELECT pg_catalog.setval('public.disk_disk_id_seq', 47, true);
 
 
 --
@@ -692,7 +763,7 @@ SELECT pg_catalog.setval('public.employee_employee_id_seq', 3, true);
 -- Name: event_event_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.event_event_id_seq', 12, true);
+SELECT pg_catalog.setval('public.event_event_id_seq', 25, true);
 
 
 --
@@ -828,25 +899,19 @@ CREATE INDEX idx_name_combo ON public.combo USING btree (combo_name);
 
 
 --
--- Name: comboinorder points_added; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER points_added AFTER INSERT ON public.comboinorder FOR EACH ROW EXECUTE FUNCTION public.calculate_total_order_price();
-
-
---
--- Name: diskinorder points_added; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER points_added AFTER INSERT ON public.diskinorder FOR EACH ROW EXECUTE FUNCTION public.calculate_total_order_price();
-
-
---
 -- Name: Order Order_customer_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public."Order"
     ADD CONSTRAINT "Order_customer_id_fkey" FOREIGN KEY (customer_id) REFERENCES public.customer(customer_id);
+
+
+--
+-- Name: Order Order_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Order"
+    ADD CONSTRAINT "Order_event_id_fkey" FOREIGN KEY (event_id) REFERENCES public.event(event_id);
 
 
 --
