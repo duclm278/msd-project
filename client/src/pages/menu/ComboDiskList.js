8016ac57b9d2b44f87d31a454e6d7a41b446610e
duclm278 <palered548@gmail.com>
2023-02-16 11:43:14 +0700

    Fix layout combo in menu

--- client/src/pages/menu/ComboDiskList.js
+++ client/src/pages/menu/ComboDiskList.js
@@ -12,83 +12,79 @@ import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import React from "react";
 
 export default function ComboDiskList({
-    diskList,
-    setSelectedDisks,
-    selectedDisks,
+  diskList,
+  setSelectedDisks,
+  selectedDisks,
 }) {
-    console.log(selectedDisks);
-    return (
-        <Stack py={2} spacing={2}>
-            {diskList.map((diskItem, index) => (
-                <React.Fragment key={index}>
-                    <FormControl>
-                        <Stack
-                            direction={"row"}
-                            justifyContent={"space-between"}
-                            alignItems={"center"}
-                            sx={{
-                                borderBottom: "1px solid",
-                                borderColor: "background.level2",
-                            }}
-                        >
-                            <Stack>
-                                <FormLabel>{diskItem.disk_name}</FormLabel>
-                                <Typography level="body3">
-                                    Price: {diskItem.price.toLocaleString()}đ
-                                </Typography>
-                            </Stack>
-                            <Stack
-                                direction="row"
-                                alignItems="end"
-                                spacing={1}
-                                marginRight={"12px"}
-                                marginBottom={"4px"}
-                            >
-                                <Box
-                                    sx={{
-                                        display: "flex",
-                                        gap: 2,
-                                        alignItems: "center",
-                                        pt: 1,
-                                    }}
-                                >
-                                    <IconButton
-                                        size="sm"
-                                        variant="outlined"
-                                        onClick={() => {
-                                            const itemIndex =
-                                                selectedDisks.findIndex(
-                                                    (item) =>
-                                                        item.id ===
-                                                        diskItem.disk_id
-                                                );
+  console.log(selectedDisks);
+  return (
+    <Stack py={2} spacing={2}>
+      {diskList.map((diskItem, index) => (
+        <React.Fragment key={index}>
+          <FormControl>
+            <Stack
+              direction={"row"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              sx={{
+                borderBottom: "1px solid",
+                borderColor: "background.level2",
+              }}
+            >
+              <Stack>
+                <FormLabel>{diskItem.disk_name}</FormLabel>
+                <Typography level="body3">
+                  Price: {diskItem.price.toLocaleString()}đ
+                </Typography>
+              </Stack>
+              <Stack
+                direction="row"
+                alignItems="end"
+                spacing={1}
+                marginRight={"12px"}
+                marginBottom={"4px"}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: 2,
+                    alignItems: "center",
+                    pt: 1,
+                  }}
+                >
+                  <IconButton
+                    size="sm"
+                    variant="outlined"
+                    onClick={() => {
+                      const itemIndex = selectedDisks.findIndex(
+                        (item) => item.id === diskItem.disk_id
+                      );
 
-                                            if (itemIndex === -1) {
-                                                setSelectedDisks((prev) => [
-                                                    ...prev,
-                                                    {
-                                                        id: diskItem.disk_id,
-                                                        quantity: 1,
-                                                    },
-                                                ]);
-                                            } else {
-                                                setSelectedDisks((prev) => {
-                                                    prev[
-                                                        itemIndex
-                                                    ].quantity += 1;
-                                                    return [...prev];
-                                                });
-                                            }
-                                        }}
-                                    >
-                                        <AddRoundedIcon />
-                                    </IconButton>
-                                </Box>
-                            </Stack>
-                        </Stack>
-                    </FormControl>
-                </React.Fragment>
-            ))}
-        </Stack>
-    );
+                      if (itemIndex === -1) {
+                        setSelectedDisks((prev) => [
+                          ...prev,
+                          {
+                            id: diskItem.disk_id,
+                            quantity: 1,
+                            price: diskItem.price,
+                          },
+                        ]);
+                      } else {
+                        setSelectedDisks((prev) => {
+                          prev[itemIndex].quantity += 1;
+                          return [...prev];
+                        });
+                      }
+                    }}
+                  >
+                    <AddRoundedIcon />
+                  </IconButton>
+                </Box>
+              </Stack>
+            </Stack>
+          </FormControl>
+        </React.Fragment>
+      ))}
+    </Stack>
+  );
 }
